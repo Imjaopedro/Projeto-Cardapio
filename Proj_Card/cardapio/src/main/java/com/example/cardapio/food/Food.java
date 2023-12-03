4bb827fb651453afb69be52fa08030d72ffe5a86
Imjaopedro <125218310+Imjaopedro@users.noreply.github.com>
2023-12-02 23:28:13 -0300

    Add files via upload

---
+++ Proj_Card/cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,64 @@
+package com.example.cardapio.food;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+
+
+    public Food(FoodRequestDTO data){
+
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
+
